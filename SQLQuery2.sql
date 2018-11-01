bb677c56710a1657e43e91c9e28ad0d2935320ce
ebventurino <38991970+ebventurino@users.noreply.github.com>
2018-11-01 09:44:00 -0500

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,7 +0,0 @@
-﻿INSERT INTO Artwork Values ('Awakening',1500.00,'Oil on Canvas',1,'515ed090-3230-4831-b957-335ce5ad3de1');
-INSERT INTO Artwork Values ('When Words Fail',1000.00,'Oil on Canvas',1,'515ed090-3230-4831-b957-335ce5ad3de1');
-INSERT INTO Artwork Values ('Deluge',1000.00,'Acrylic on Canvas',1,'515ed090-3230-4831-b957-335ce5ad3de1');
-
-INSERT INTO ArtType Values ('Painting');
-INSERT INTO ArtType Values ('Drawing');
-INSERT INTO ArtType Values ('Sculpture');
