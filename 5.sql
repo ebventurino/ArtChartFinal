132dd0133ef2be1e1bc0b9fcdac3e39a52a42389
ebventurino <38991970+ebventurino@users.noreply.github.com>
2018-11-01 09:44:10 -0500

    Delete 5.sql

--- 5.sql
+++
@@ -1,3 +0,0 @@
-﻿INSERT INTO Artwork Values ('Awakening',1500.00,'Oil on Canvas',1,'515ed090-3230-4831-b957-335ce5ad3de1');
-INSERT INTO Artwork Values ('When Words Fail',1000.00,'Oil on Canvas',1,'515ed090-3230-4831-b957-335ce5ad3de1');
-INSERT INTO Artwork Values ('Deluge',1000.00,'Acrylic on Canvas',1,'515ed090-3230-4831-b957-335ce5ad3de1');
